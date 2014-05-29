df9c669c18f791d713be759accd85fa2f3098100
Moritz Gunz <moritz.gunz@gmail.com>
2014-05-29 10:24:44 +0200

    Added project and first classes

---
+++ src/UpNet.Core/Updater.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UpNet.Core.DataSource;
+
+namespace UpNet.Core
+{
+    public class Updater
+    {
+        public IDataSource DataSource { get; private set; }
+
+        public async Task<bool> CheckForUpdate()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task UpdateAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
